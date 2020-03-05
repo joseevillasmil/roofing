d9bc6a3a05bcadafa834f4e38de3ff484a68b06d
Jose Villasmil <jose.villasmil@doctum.cl>
2020-03-05 01:54:05 -0300

    Update agreement.blade.php

--- resources/views/pdf/agreement.blade.php
+++ resources/views/pdf/agreement.blade.php
@@ -4,18 +4,18 @@
             <img src="{{public_path('img/logo.png')}}" width="130" >
         </td>
         <td>
-            <p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; TEXAS GOLD ROOFING &amp; MORE</strong></p>
-            <p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10718 Lonesome Dove Ct, Cypress, TX 77095</strong></p>
-            <p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Ph.&nbsp;&nbsp;(346)420-9716 / (713)366-9700</strong></p>
-            <p><strong>&nbsp;</strong></p>
-            <p><strong>&nbsp;</strong></p>
+            <p><strong> TEXAS GOLD ROOFING &amp; MORE</strong></p>
+            <p><strong> 10718 Lonesome Dove Ct, Cypress, TX 77095</strong></p>
+            <p><strong> Ph.(346)420-9716 / (713)366-9700</strong></p>
+            <p><strong></strong></p>
+            <p><strong></strong></p>
         </td>
     </tr>
 </table>
 
 <h1>AUTHORIZATION AGREEMENT</h1>
 <p>I/We, owner of the property located <u>{{$request->address}}</u></p>
-<p>&nbsp;</p>
+<p></p>
 <p>Property owner agrees that TEXAS GOLD ROOFING will perform the installations, or repairs to the said property in accordance with the insurance company&rsquo;s damage and work scope. The property owner will pay to TEXAS GOLD ROOFING all checks from the insurance company, including:</p>
 <ol>
     <li>The initial check (ACV) that comes with the detailed work scope from the insurance company. At this time property owner will be assisted in selecting any available options. TEXAS GOLD ROOFING COMPANY then orders all materials, schedules work to be done, and orders all inspections during and after repairs and</li>
@@ -23,37 +23,30 @@
     <li>All supplemental funds from the insurance company for unforeseen costs. If these occur, they will be negotiated by TEXAS GOLD ROOFING and they are paid by the insurance</li>
     <li>Any upgrade that is selected will due upon</li>
 </ol>
-<p>&nbsp;</p>
+<p></p>
 <p><strong>Texas law requires a person insured under a property insurance policy to pay any deductible applicable to a claim made under the policy. It is a violation of this Texas law for a person or business paid wholly or partly from proceeds of a property insurance claim to knowingly allow the insured person to fail to pay. Or assist the insured person&rsquo;s failure to pay, the applicable insurance deductible. </strong></p>
-<p><strong>&nbsp;</strong></p>
+<p><strong></strong></p>
 <p><strong>I understand that by initialing and signing this agreement I am bound by the terms of this contract and testify that I am the owner, or acting on behalf of the owner, spouse or landlord. ________</strong></p>
-<p><strong>&nbsp;</strong></p>
+<p><strong></strong></p>
 <p><strong>If owner cancels the agreement the owner agrees to pay twenty five percent (25%) of the total amount authorized by owner's insurance company, for the administrative and technical expenses incurred. _____</strong></p>
-<p>&nbsp;</p>
-<p><strong>Name:</strong> {{$request->name}} &nbsp;<strong>DOB:{{$request->birthday}}&nbsp;&nbsp;&nbsp; 4SS:</strong> {{$request->ss}}</p>
-<p><strong>Address: </strong> {{$request->address}} <strong>City {{$request->city}} Zip</strong>: {{$request->zip}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>Home Phone:</strong> {{$request->telephone}} <strong>Cell Phone: &nbsp;</strong>{{$request->cellphone}} <strong>Email: </strong>{{$request->email}}&nbsp;</p>
+<p></p>
+<p><strong>Name:</strong> {{$request->name}} <strong>DOB:{{$request->birthday}} 4SS:</strong> {{$request->ss}}</p>
+<p><strong>Address: </strong> {{$request->address}} <strong>City {{$request->city}} Zip : {{$request->zip}} <strong>Home Phone:</strong> {{$request->telephone}} <strong>Cell Phone: </strong>{{$request->cellphone}} <strong>Email: </strong>{{$request->email}}</p>
 <p><strong>Insurance Company/Agent: </strong>{{$request->agent}} <strong>Policy Number: </strong>{{$request->policy_number}}</p>
-<p><strong>Rep Phone: &nbsp;</strong>____________________________ <strong>Rep. Signature: &nbsp;</strong>____________________________<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong></p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>Policy Holder Signature:</strong> _____________________________<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Date:&nbsp; {{date('m')}}/{{date('d')}}/{{date('Y')}}</strong>&nbsp;&nbsp;&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
 <p><strong><u>Terms and conditions</u></strong></p>
-<p>&nbsp;</p>
+<p></p>
 <p>Owner agrees to authorize contractor to use the electricity of the property to complete their work under this agreement. He/She understands that all picture frames and all wall accessories that are not properly secure must be removed until all work is completed.</p>
-<p>&nbsp;</p>
-<p>Plants will be relocated or trimmed prior to the beginning of the work.&nbsp; Owner grants contractor to work during day light hours from Monday thru Saturday between 6:00 AM to 9:00 PM. Contractor is not responsible by delays caused by strikes, weather, accidents or other events beyond contractor's control, except through contractor's negligence. Contractor will not be held responsible for damages of interior fixtures or drywall. Owner agrees to grant contractor the right to display advertising signs on the premise from the day of the contracting through 30 days following completion of the work.</p>
-<p>&nbsp;</p>
+<p></p>
+<p>Plants will be relocated or trimmed prior to the beginning of the work. Owner grants contractor to work during day light hours from Monday thru Saturday between 6:00 AM to 9:00 PM. Contractor is not responsible by delays caused by strikes, weather, accidents or other events beyond contractor's control, except through contractor's negligence. Contractor will not be held responsible for damages of interior fixtures or drywall. Owner agrees to grant contractor the right to display advertising signs on the premise from the day of the contracting through 30 days following completion of the work.</p>
+<p></p>
 <p>Contractor shall be entitled to receive payment issued by insurance company for the ordering of the material before the commencement of the work. After the contractor has declared the work to be satisfactorily performed, contractor must immediately be paid the remaining of the balance due. Contractor shall have the right to stop the job and keep the job idle if the payment in accordance to this agreement is not made. In the event owner does not pay according to the terms of this agreement, contractor may add a monthly interest fee allowed by the law of Texas.</p>
-<p>&nbsp;</p>
+<p></p>
 <p>All jobs performed by company have 8 years labor warranty. It is specifically understood between both parties hereto that in the event of a dispute over any of the terms contained in this agreement, the Texas district court should have full and exclusive jurisdiction over the matter. Owner shall be responsible for any court cost and attorney fees incurred by contractor. This contract is subject to the Chapter 27th of the Texas Property Code. If you have a complain concerning a construction defect arising from the performance of this contract and defect has not been corrected through normal warranty service, you must provide the notice required in Chapter 27th to the contractor by certified mail and return receipt requested not later than 60 days before you file notice must describe the construction defect and provided by section 27.004 of the Texas Properly Code.</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+<p></p>
+<p></p>
+<p></p>
 <p>I/We agree  <br>
     @if($request->sign)
-        <img src="{{public_path('img/' . $request->sign)}}" width="120">
+        <img src="{{public_path('img/' . $request->sign)}}" width="250">
     @endif
 </p>
