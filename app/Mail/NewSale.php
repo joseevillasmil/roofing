b98d43241f0e3dcd833aa7c2189b93fa011843cb
Jose Villasmil <jose.villasmil@doctum.cl>
2020-03-05 23:44:05 -0300

    Update NewSale.php

--- app/Mail/NewSale.php
+++ app/Mail/NewSale.php
@@ -29,10 +29,18 @@ class NewSale extends Mailable
      */
     public function build()
     {
-        return $this->view('mails/new-sale')
+
+       $mail =  $this->view('mails/new-sale')
                     ->subject('New sale registred')
                     ->from(env('MAIL_FROM'))
                     ->attach(storage_path('app/agreements/' . $this->sale->agreement))
                     ->with('sale',  $this->sale);
+
+        if($this->sale->images) {
+            foreach($this->sale->images as $image) {
+                $mail->attach(public_path('img/' . $image));
+            }
+        }
+        return $mail;
     }
 }
